7ea39fdb9915fb8478daafc25d6d8caf210029be
gtjames <gtjames@gmail.com>
2022-04-11 10:59:28 -0500

    Added User Pool keys

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_jZ530rqFB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '22e0mjlke5rt77buglfe1grv1p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-west-1_wwJLDIHdn',
+        userPoolClientId: '2g5hnpg0la2gi9045f3pveh1ts',
+        region: 'us-west-1'
     },
     api: {
         invokeUrl: 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
