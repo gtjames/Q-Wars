e9d314f77c3385fbbdada41602b9d4d377e38653
gtjames <gtjames@gmail.com>
2022-04-12 11:12:10 -0500

    added in the Fighting words

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_wwJLDIHdn',
-        userPoolClientId: '2g5hnpg0la2gi9045f3pveh1ts',
-        region: 'us-west-1'
+        userPoolId:         'us-west-1_wwJLDIHdn',
+        userPoolClientId:   '2g5hnpg0la2gi9045f3pveh1ts',
+        region:             'us-west-1'
     },
     api: {
-        invokeUrl: 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ' https://slcrbpag33.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
